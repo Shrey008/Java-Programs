371cb8ffdc3da4e25e4b61dd865ce5c7bc706377
Shrey008 <115262845+Shrey008@users.noreply.github.com>
2023-08-28 00:26:27 +0530

    Add files via upload

---
+++ aoc.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class aoc {
+ public static void main(String[]args)
+ {
+
+     int radius = 4;
+     double pi = 3.14,area;
+     area = 3.14 *radius *radius;
+     System.out.println("area is:"+area);
+ }
+}
