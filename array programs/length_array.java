3483716537d5c9b37830a740e416d566348d4fdc
Shrey008 <115262845+Shrey008@users.noreply.github.com>
2023-09-12 01:16:14 +0530

    Add files via upload

---
+++ array programs/length_array.java
@@ -0,0 +1,10 @@
+public class length_array {
+    public static void main(String[] args) {
+        int [] arr = {10,7,6,5,9,30,56,67,78,89};
+      //  System.out.println(arr.length) this code returns length ;
+        for(int i = 0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
